60f1b41c2a5c015278bec6f65744c82f9c8c57e7
Alex H <alexhughes1805@gmail.com>
2025-12-04 14:18:20 +0000

    testing random command outputs

--- commands/text/test.js
+++ commands/text/test.js
@@ -1,11 +1,20 @@
 const {SlashCommandBuilder} = require("discord.js");
 
+const Responses = [
+    "image 1",
+    "image 2",
+    "image 3",
+    "image 4",
+    "image 5"
+];
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('test')
 		.setDescription('Testing the bot'),
 	async execute(interaction)
     {
-		await interaction.reply("Who up testing rn");
+		const Response = Math.floor(Math.random() * Responses.length);
+		await interaction.reply(`${Responses[Response]}`);
 	},
 };
