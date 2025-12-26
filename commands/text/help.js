8586cee1bd4f0bd90f48349db80aa49e9ce9407a
Alex H <alexhughes1805@gmail.com>
2025-12-26 15:40:08 +0000

    added extra paragrah in /help

--- commands/text/help.js
+++ commands/text/help.js
@@ -6,6 +6,6 @@ module.exports = {
 		.setDescription('Shows available commands and their descriptions'),
 	async execute(interaction)
     {
-		await interaction.reply("**\/tomb** - Enter a tomb by yourself or with others. Tag others to have them join you. \n**\/inspectchest** - Open a chest in a tomb for an item. Can only be used if \/tomb was used first. \n**\/exit** - Leave a tomb by yourself or with others. Tag others to have them join you. \n**\/inventory** - Lists items you have collected. \n**\/give **- Give an item to another person. Tag them to give them the item. \n**\/consume **- Consumes an item in your inventory. \n**/profile** - Lists items that have been consumed.\n Link to the documentation: [here](https://docs.google.com/document/d/1tmNFFXo34ELh7KZDmYiPH_q7yvZLxRJrUYqQunyRrMM/edit?usp=sharing)");
+		await interaction.reply("**\/tomb** - Enter a tomb by yourself or with others. Tag others to have them join you. \n**\/inspectchest** - Open a chest in a tomb for an item. Can only be used if \/tomb was used first. \n**\/exit** - Leave a tomb by yourself or with others. Tag others to have them join you. \n**\/inventory** - Lists items you have collected. \n**\/give **- Give an item to another person. Tag them to give them the item. \n**\/consume **- Consumes an item in your inventory. \n**/profile** - Lists items that have been consumed.\n\n Link to the documentation: [here](https://docs.google.com/document/d/1tmNFFXo34ELh7KZDmYiPH_q7yvZLxRJrUYqQunyRrMM/edit?usp=sharing)");
     },
 };
