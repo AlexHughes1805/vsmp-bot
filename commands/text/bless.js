51f7e7f20c1c9aec316f500c9959aa5cb71c9146
Alex H <alexhughes1805@gmail.com>
2026-01-29 23:39:39 +0000

    Small fix in formatting

--- commands/text/bless.js
+++ commands/text/bless.js
@@ -30,7 +30,7 @@ module.exports = {
 
         else
         {
-            await interaction.reply(`You have created holy water.`)
+            await interaction.reply(`You have created holy water.`);
 
             let userInventory = await inventory.findOne({ userID: userID });
                         
